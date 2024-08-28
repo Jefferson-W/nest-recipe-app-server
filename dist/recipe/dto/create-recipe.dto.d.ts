8c686ac4332fc3443ae820740c6eda7fcd64be36
jefferson.willian <jefferson.lsilva@me.com.br>
2024-08-28 17:07:01 -0300

    fix: removing dist paste

--- dist/recipe/dto/create-recipe.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateRecipeDto {
-    name: string;
-    email: string;
-    authorization: string;
-    authHeader: any;
-    uid: any;
-}
--- dist/recipe/recipe.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateRecipeDto } from './dto/create-recipe.dto';
-export declare class RecipeService {
-    createTeste(user: CreateRecipeDto): CreateRecipeDto;
-    create(user: CreateRecipeDto): any;
-    findAll(): any[];
-    findOne(id: number): string;
-    remove(id: number): string;
-}
